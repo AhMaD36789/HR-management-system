5b6ba3a0caf0b470b143ff291e6746aee62dbda5
AhMaD36789 <ahmadsa28121999@gmail.com>
2023-04-09 07:44:50 +0000

    first lab req with bug fixed

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
 "use strict";
 
-function employee(employeeID,firstName,lastName,Department,level,imgURL,salary){
+function employee(employeeID,firstName,lastName,Department,level,imgURL){
     this.employeeID=employeeID;
     this.fullName=firstName+" "+lastName;
     this.Department=Department;
     this.level=level;
     this.imgURL=imgURL;
-    this.salary=this.taxedSalary();
 }
-employee.prototype.taxedSalary= function(){
+employee.prototype.salary= function(){
+    let temp=0;
     if (this.level=="junior"||this.level=="Junior")
     {
-        Math.floor(Math.random() * 501)+500;
-        this.salary=Math.floor(this.salary-this.salary*(7.5/100));
-        console.log(this.salary);
+        temp=Math.floor(Math.random() * 501)+500;
+        temp=Math.floor(temp-temp*(7.5/100));
+        
     }
     else if (this.level=="mid-senior"||this.level=="Mid-Senior")
     {
-        Math.floor(Math.random() * 501)+1000;
-        this.salary=Math.floor(this.salary-this.salary*(7.5/100));
-        console.log(this.salary);
+        temp=Math.floor(Math.random() * 501)+1000;
+        temp=Math.floor(temp-temp*(7.5/100));
+        
     }
     else if (this.level=="senior"||this.level=="Senior")
     {
-        Math.floor(Math.random() * 501)+1500;
-        this.salary=Math.floor(this.salary-this.salary*(7.5/100));
-        console.log(this.salary);
+        temp=Math.floor(Math.random() * 501)+1500;
+        temp=Math.floor(temp-temp*(7.5/100));
+        
     }
     
+    return temp;
 }
 
 employee.prototype.render= function(){
-    document.write(`${this.fullName} &emsp; ${this.salary} `);
+    document.write(`${this.fullName} &emsp; ${this.salary()} `);
     document.write("<br>");
 }
 const employee1000 = new employee (1000,"Ghazi","Samer","Administration","Senior","url")
@@ -41,8 +42,6 @@ const employee1003 = new employee (1003,"Safi","Walid","Administration","Mid-Sen
 const employee1004 = new employee (1004,"Omar","Zaid","Development","Senior","url")
 const employee1005 = new employee (1005,"Rana","Saleh","Development","Junior","url")
 const employee1006 = new employee (1006,"Hadi","Ahmad","Finance","Mid-Senior","url")
-
-
 
 employee1000.render();
 employee1001.render();
